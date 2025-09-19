ab367968e3b13af5efe7e407fe59b08d3b28f97e
Andrew ONeill <andy@evolve757.com>
2025-09-19 09:57:40 -0400

    edited exercise and finished

---
+++ CarLotSimulatorApp/CarLotSimulator/Carlot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarLotSimulator;
+
+public class CarLot
+{
+    public List<Car> ParkingLot {get; set;} =  new List<Car>();
+
+    public void CheckCars()
+    {
+        foreach (var car in ParkingLot)
+        {
+            Console.WriteLine($"Make: {car.Make},  Model: {car.Model}, Year: {car.Year}");
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -6,6 +6,8 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
+            var carLotOne = new CarLot();
+            
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
@@ -19,11 +21,39 @@ namespace CarLotSimulator
             firstCar.MakeEngineNoise("peaceful and quiet");
             
             firstCar.MakeHonkNoise("A fire alarm went off");
+            
+            carLotOne.ParkingLot.Add(firstCar);
+            
+            var secondCar = new Car();
+            secondCar.Make = "Mazda";
+            secondCar.Model = "Civic";
+            secondCar.Year = 2016;
+            secondCar.IsDriveable = true;
+            
+            secondCar.MakeEngineNoise("peaceful and quiet");
+            secondCar.MakeEngineNoise("A Humming Bird");
+            
+            carLotOne.ParkingLot.Add(secondCar);
+            
+            var thirdCar = new Car();
+            thirdCar.Make = "Toyota";
+            thirdCar.Model = "Camry";
+            thirdCar.Year = 1995;
+            thirdCar.IsDriveable = true;
+            
+            thirdCar.MakeEngineNoise("loud and clunky");
+            thirdCar.MakeEngineNoise("A trombone");
+            
+            carLotOne.ParkingLot.Add(thirdCar);
+
+            carLotOne.CheckCars();
         }
 
             //Create a CarLot class
             //It should have at least one property: a List of cars
             //Instanciate the Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            
     }
+    
 }
